7571fe772eec5dd14f439b1a19fb68d93a6eec89
kazu634 <simoom634@yahoo.co.jp>
2019-04-27 13:27:39 +0700

    コメントを追加 (msg-name-cgange.go)

--- msg-name-change.go
+++ msg-name-change.go
@@ -7,12 +7,16 @@ import (
 	"path/filepath"
 )
 
+// RedmineのIssueディレクトリを列挙する
 func listOfIssueDirs(dir string) []string {
+	// 引数で渡されたディレクトリを読み込み
 	files, err := ioutil.ReadDir(dir)
 	if err != nil {
 		panic(err)
 	}
 
+	// 取得したディレクトリ・ファイルのリストから、
+	// ディレクトリのみを result に格納する
 	var results []string
 	for _, file := range files {
 		if file.IsDir() {
@@ -25,20 +29,25 @@ func listOfIssueDirs(dir string) []string {
 }
 
 func main() {
+	// 引数の数チェック
 	if len(os.Args) < 1 {
 		os.Exit(1)
 	}
 
+	// 引数が存在するディレクトリかどうかを確認する
 	root := os.Args[1]
 	if _, err := os.Stat(root); err != nil {
 		fmt.Printf("%v\n", err)
 		os.Exit(2)
 	}
 
+	// Redmineのissueに該当するディレクトリを列挙する
 	issueDirs := listOfIssueDirs(root)
+	// issueディレクトリ配下の*.msgに対して
 	for _, issueDir := range issueDirs {
 		target := filepath.Join(issueDir, "*.msg")
 
+		// 一括でファイル名を変換する
 		mails, _ := filepath.Glob(target)
 		for index, src := range mails {
 			target := filepath.Join(issueDir, fmt.Sprintf("%07d.msg", index))
